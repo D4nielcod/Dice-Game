c6adfbc3e4f70be2e6647a2161c4f591c3360849
D4nielcod <thorbilol96@gmail.com>
2020-11-22 23:23:12 +0100

    First Commit

---
+++ playerOne.js
@@ -0,0 +1,18 @@
+alert("Player One's turn");
+
+// for the first dice
+var imageGotten = document.querySelectorAll('.first');
+imageArray = ['dice1.png','dice2.png','dice3.png','dice4.png','dice5.png','dice6.png'];
+
+document.getElementById('myBtn').onclick = function changeContent() {
+  newArray = ['dice1.png','dice2.png','dice3.png','dice4.png','dice5.png','dice6.png'].sort(function() {
+    return .5 - Math.random();
+  });
+
+  a = document.querySelectorAll('.first')[0].src = newArray[0];
+  b = document.querySelectorAll('.first')[1].src = newArray[1];
+
+  if (a=='dice1.png') {
+
+  }
+}
